23274bfec1b4836d1c1285283a2a482e277ed72f
mahesh-wagh <maheshbwagh@gmail.com>
2021-05-31 18:23:24 +0530

    Integrated aspose changes

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -16,6 +16,8 @@ using NPOI.XSSF.UserModel;
 using GemBox.Spreadsheet;
 using System.Data.OleDb;
 using System.Runtime.Versioning;
+using Aspose.Cells.Cloud;
+using Aspose.Cells;
 
 namespace WebProject.Controllers
 {
@@ -78,7 +80,7 @@ namespace WebProject.Controllers
             try
             {
                 ExcelEngine excelEngine = new ExcelEngine();
-               // string FilePath = _env.ContentRootPath + "\\" + "Bulk Records.xlsx";
+                // string FilePath = _env.ContentRootPath + "\\" + "Bulk Records.xlsx";
                 using (var stream = new FileStream(filename, FileMode.Open, FileAccess.Read))
                 {
                     var workbook = excelEngine.Excel.Workbooks.Open(stream);
@@ -152,6 +154,82 @@ namespace WebProject.Controllers
                 return Ok(ex.Message + " " + ex.StackTrace.ToString());
             }
         }
+
+        [HttpGet]
+        [Route("getexcel")]
+        public IActionResult GetExcelFromDataTable()
+        {
+            try
+            {
+                //create a excel file
+                
+                //Instantiating a Workbook object
+                Workbook workbook = new Workbook();
+
+                //Adding a new worksheet to the Workbook object
+                int i = workbook.Worksheets.Add();
+
+                //Obtaining the reference of the newly added worksheet by passing its sheet index
+                Worksheet worksheet = workbook.Worksheets[i];
+                //Instantiating a "Products" DataTable object
+                DataTable dataTable = new DataTable("Products");
+
+                //Adding columns to the DataTable object
+                dataTable.Columns.Add("Product ID", typeof(Int32));
+                dataTable.Columns.Add("Product Name", typeof(string));
+                dataTable.Columns.Add("Units In Stock", typeof(Int32));
+
+                //Creating an empty row in the DataTable object
+                DataRow dr = dataTable.NewRow();
+
+                //Adding data to the row
+                dr[0] = 1;
+                dr[1] = "Aniseed Syrup";
+                dr[2] = 15;
+
+                //Adding filled row to the DataTable object
+                dataTable.Rows.Add(dr);
+
+                //Creating another empty row in the DataTable object
+                dr = dataTable.NewRow();
+
+                //Adding data to the row
+                dr[0] = 2;
+                dr[1] = "Boston Crab Meat";
+                dr[2] = 123;
+
+                //Adding filled row to the DataTable object
+                dataTable.Rows.Add(dr);
+
+                //Importing the contents of DataTable to the worksheet starting from "A1" cell,
+                //where true specifies that the column names of the DataTable would be added to
+                //the worksheet as a header row
+                worksheet.Cells.ImportDataTable(dataTable, true, "A1");
+                var fileName = DateTime.Now.ToString("yyyyMMddhhmmss");
+                workbook.Save(fileName + ".xls");
+
+
+                Workbook workbookR = new Workbook(fileName + ".xls");
+
+                // Using the Sheet 1 in Workbook
+                Worksheet worksheetR = workbook.Worksheets[0];
+
+                // Accessing a cell using its name
+                Cell cell = worksheet.Cells["A1"];
+
+                string value = cell.Value.ToString();
+
+
+                //read an excel file
+
+                return Ok(value);
+            }
+            catch (Exception ex)
+            {
+                return Ok(ex.Message + " " + ex.StackTrace.ToString());
+            }
+        }
+
         [HttpGet]
         [Route("oledb")]
         public IActionResult GetReadingFromOLEDB(string filename)
